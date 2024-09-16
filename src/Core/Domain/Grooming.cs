5955be17c4103c52f6cfd511cf9ea2849e0294d7
amirholya01 <amirholya01@gmail.com>
2024-09-16 15:39:10 +0200

    Grooming Entity

---
+++ src/Core/Domain/Grooming.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class Grooming : BaseEntity
+    {
+        public string Services { get; set; }
+        public DateTime DateTime { get; set; }
+    }
+}
