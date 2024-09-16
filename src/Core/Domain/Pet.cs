14e1a80f5f414093a5d57aa3b2a15ab0a177c02a
amirholya01 <amirholya01@gmail.com>
2024-09-16 15:32:37 +0200

    Pet Entity

---
+++ src/Core/Domain/Pet.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class Pet : BaseEntity
+    {
+        public string Name { get; set; }
+        public string Breed { get; set; }
+        public int Age { get; set; }
+        public double Weight { get; set; }
+    }
+}
