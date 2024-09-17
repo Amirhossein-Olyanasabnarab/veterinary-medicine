7eaefbc2b574b2f8706f6b89994a51f0c6da05b5
amirholya01 <amirholya01@gmail.com>
2024-09-17 21:02:46 +0200

    Client-Refactoring

--- src/Core/Domain/Appointment.cs
+++ src/Core/Domain/Appointment.cs
@@ -9,12 +9,11 @@ namespace Domain
     public class Appointment : BaseEntity
     {
         public DateTime DateTime { get; set; }
-        public decimal Fee { get; set; }
+        
         public int ClientId { get; set; }
         public Client Client { get; set; }
         public int VetId { get; set; }
         public Vet Vet { get; set; }
-        public int PetId { get; set; }
-        public Pet Pet { get; set; }
+    
     }
 }
--- src/Core/Domain/User.cs
+++ src/Core/Domain/User.cs
@@ -10,6 +10,6 @@ namespace Domain
     {
         public string Username { get; set; }
         public string Password { get; set; }
-
+        public Role Role { get; set; }
     }
 }
