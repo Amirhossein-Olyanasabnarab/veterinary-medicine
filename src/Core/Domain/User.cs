27d9cc654315e7fa7d6c2cb7c5134f8e2619fdad
amirholya01 <amirholya01@gmail.com>
2024-09-17 21:12:14 +0200

    Role-Refactoring

--- src/Core/Domain/User.cs
+++ src/Core/Domain/User.cs
@@ -10,6 +10,8 @@ namespace Domain
     {
         public string Username { get; set; }
         public string Password { get; set; }
-        public Role Role { get; set; }
+
+        public int RoleId { get; set; }  // Foreign Key
+        public Role Role { get; set; } // Navigation Property
     }
 }
