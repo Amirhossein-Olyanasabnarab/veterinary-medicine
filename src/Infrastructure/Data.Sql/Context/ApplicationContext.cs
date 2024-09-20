631c3701827eccc56e9f2713f235cb91e40c9bf7
amirholya01 <amirholya01@gmail.com>
2024-09-20 07:58:47 +0200

    applicatio context

---
+++ src/Infrastructure/Data.Sql/Context/ApplicationContext.cs
@@ -0,0 +1,29 @@
+﻿using Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Sql.Context
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options)
+        {
+            
+        }
+
+
+
+        public DbSet<AboutUs> AboutUs { get; set; }
+        public DbSet<Appointment> Appointments { get; set; }
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<Grooming> Groomings { get; set; }
+        public DbSet<Pet> Pets { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Vet> Vets { get; set; }
+    }
+}
