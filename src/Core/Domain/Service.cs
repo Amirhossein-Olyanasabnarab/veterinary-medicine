d8dd51cc22c7ef8b17970ea0eadfbf285327b584
amirholya01 <amirholya01@gmail.com>
2024-09-23 08:30:05 +0200

    Appintment-Updated

--- src/Core/Domain/Service.cs
+++ src/Core/Domain/Service.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Domain.Enums;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,10 @@ using System.Threading.Tasks;
 
 namespace Domain
 {
-    internal class Service
+    public class Service : BaseEntity
     {
+        public ServiceType  Type { get; set; }
+        public decimal Price { get; set; }
+        public string Description { get; set; }
     }
 }
