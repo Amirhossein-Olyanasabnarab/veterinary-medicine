9e06d589c77a39181dd41a2f4599bc1e42636087
amirholya01 <amirholya01@gmail.com>
2024-09-26 12:54:37 +0200

    IUnitOfWork and UnitOfWork class

---
+++ src/Infrastructure/Data.Sql/UnitOfWork.cs
@@ -0,0 +1,25 @@
+﻿using Contracts;
+using Data.Sql.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Sql
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ApplicationContext _context;
+        public UnitOfWork(ApplicationContext context)
+        {
+            _context = context;
+        }
+        public IVetRepository VetRepository { get ; set; }
+
+        public void SaveChange()
+        {
+            _context.SaveChanges();
+        }
+    }
+}
